71f8d515e837e42496194dfdc8f61fe7df136c0d
Selinski15 <108267897+Selinski15@users.noreply.github.com>
2022-07-28 19:13:04 +0200

    Add files via upload

---
+++ Script test1.sql
@@ -0,0 +1,12 @@
+SELECT * FROM publishers;
+authors
+;
+publishers;
+
+SELECT pub_name, state
+FROM publishers;
+SELECT *
+FROM publishers
+WHERE city = 'New York' OR city = 'Boston';
+SELECT DISTINCT city 
+FROM publishers;
